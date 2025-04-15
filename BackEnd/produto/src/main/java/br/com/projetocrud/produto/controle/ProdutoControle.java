087820dc11890571cda0f51f363ca6f9e5a0a38c
MatoTheus18 <matheus.goncalves18@aluno.ce.gov.br>
2025-04-15 07:40:21 -0300

    CORS resolvido!

--- BackEnd/produto/src/main/java/br/com/projetocrud/produto/controle/ProdutoControle.java
+++ BackEnd/produto/src/main/java/br/com/projetocrud/produto/controle/ProdutoControle.java
@@ -2,6 +2,7 @@ package br.com.projetocrud.produto.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import br.com.projetocrud.produto.modelo.RespostaModelo;
 import br.com.projetocrud.produto.servico.ProdutoServico;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProdutoControle {
 
     @Autowired
